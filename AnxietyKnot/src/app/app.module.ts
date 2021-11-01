f8288b8f7620ba0ddd25ae5fcbe528f31c862e08
OliverPo666 <55895574+OliverPo666@users.noreply.github.com>
2021-10-31 23:13:51 -0400

    initial UI, working on quill editor and displaying charts on mood tracker

--- AnxietyKnot/src/app/app.module.ts
+++ AnxietyKnot/src/app/app.module.ts
@@ -1,24 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatCommonModule } from '@angular/material/core';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { JournalBookComponent } from './journal-book/journal-book.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { ResourceComponent } from './resource/resource.component';
+import { TrackerComponent } from './tracker/tracker.component';
+import { EntryComponent } from './entry/entry.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    JournalBookComponent,
+    ResourceComponent,
+    TrackerComponent,
+    EntryComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +45,13 @@ import { MatCommonModule } from '@angular/material/core';
     MatToolbarModule,
     MatExpansionModule,
     FormsModule,
-    MatCommonModule
+    MatCommonModule,
+    LayoutModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
