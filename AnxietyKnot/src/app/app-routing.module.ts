0708ca03a40b56c2cdf14ce401059c4a5f4fa464
Jacob Hornung <hornunjb@mail.uc.edu>
2021-11-10 23:17:08 -0500

    access to old routes for testing

--- AnxietyKnot/src/app/app-routing.module.ts
+++ AnxietyKnot/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { EntryComponent } from './entry/entry.component';
 import { JournalBookComponent } from './journal-book/journal-book.component';
 import { ResourceComponent } from './resource/resource.component';
 import { TrackerComponent } from './tracker/tracker.component';
+import { PostCreateComponent } from './post-create/post-create.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 const routes: Routes = [
   {path:'journalBook', component:JournalBookComponent},
   {path:'resource', component:ResourceComponent},
   {path:'tracker', component:TrackerComponent},
-  {path:'entry', component:EntryComponent}
+  {path:'entry', component:EntryComponent},
+  {path:'list', component:PostListComponent},
+  {path:'create', component:PostCreateComponent}
 ];
 
 @NgModule({
